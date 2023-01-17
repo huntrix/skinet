012c258dc00894be7176b8833933cfcff3e800c3
Daciana Ille <florin_blo@hotmail.com>
2023-01-18 01:23:11 +0200

    Initial Commit

---
+++ Infrastucture/Data/Migrations/20221219222319_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastucture.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    //Name = table.Column<string>(type: "INTEGER", nullable: true)
+                    Name = table.Column<string>(type: "VARCHAR(255)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
